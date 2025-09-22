1a727b7c7bd9d5fcd24ce74dd823addcb4e0ec6d
ma2345666 <mariaclarablaschi@gmail.com>
2025-09-22 13:19:04 -0300

    Add files via upload

---
+++ 9_aula/principal.py
@@ -0,0 +1,17 @@
+from Dev import Dev
+from Pessoa import Pessoa
+
+print ("Cadastro de uma nova pessoa")
+
+p = Pessoa("Antônio Bernardo","123.456.789-00","toni123@gmail.com")
+
+p.apresentarSe()
+
+print(50*"_")
+
+d = Dev("Júlia Alves","321.654.876-99","juliaalves2105@gmail.com")
+
+d.apresentarSe()
+
+print(50*"_")
+
